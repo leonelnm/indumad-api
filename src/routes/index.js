8c5ce7aa838092b7da530df285dbb50cdbf7d9e2
Erik Navarrete <elerickmontenegro@gmail.com>
2022-07-17 11:28:00 +0200

    añadir endpoint /role

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import authRouter from './auth.router.js'
+import roleRouter from './role.router.js'
 import userRouter from './user.router.js'
 
 const router = Router()
 
 router.use('/auth', authRouter)
+router.use('/role', roleRouter)
 router.use('/user', userRouter)
 
 export default router
