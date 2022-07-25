7651d578e5688919014594f2c47741f35240f539
Erik Navarrete <elerickmontenegro@gmail.com>
2022-07-25 15:55:25 +0200

    roles authorized by role

--- src/helper/utils.js
+++ src/helper/utils.js
@@ -1,3 +1,5 @@
+import { RoleEnumType } from '../types/roleEnumType.js'
+
 export const getMsgErrorJoiValidation = error => error ? error.message : undefined
 
 // Validará el rol mínimo con el que se puede acceder a la ruta
@@ -10,4 +12,27 @@ export const validateRole = ({ userRoles = [], authorizedRoles = [] }) => {
     }
   }
   return authorized
+}
+
+export const authorizedRoles = {
+  administrador: [
+    RoleEnumType.ADMINISTRADOR,
+    RoleEnumType.SUPERADMIN
+  ],
+  autonomo: [
+    RoleEnumType.AUTONOMO,
+    RoleEnumType.ADMINISTRADOR,
+    RoleEnumType.SUPERADMIN
+  ],
+  contratado: [
+    RoleEnumType.CONTRATADO,
+    RoleEnumType.ADMINISTRADOR,
+    RoleEnumType.SUPERADMIN
+  ],
+  gestor: [
+    RoleEnumType.GESTOR,
+    RoleEnumType.ADMINISTRADOR,
+    RoleEnumType.SUPERADMIN
+  ],
+  superAdmin: [RoleEnumType.SUPERADMIN]
 }
