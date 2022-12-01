428c198e8ea2fa532830c2735c38cd31e1ffb476
Erik Navarrete <elerickmontenegro@gmail.com>
2022-12-01 20:33:25 +0100

    use /tmp

--- src/config/multer.js
+++ src/config/multer.js
@@ -5,7 +5,7 @@ import { ValidationError } from '../exceptions/ValidationError.js'
 // const storage = multer.memoryStorage()
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads')
+    cb(null, '/tmp')
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now())
